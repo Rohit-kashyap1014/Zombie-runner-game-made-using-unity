ebf846d9fbbaecd88f98a08c8f9f154282fe3ba5
Rohit-kashyap1014 <40514216+Rohit-kashyap1014@users.noreply.github.com>
2018-11-16 19:52:04 +0530

    Add files via upload

---
+++ Scripts/ClearArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearArea : MonoBehaviour {
+
+	public float timeSinceLastTrigger = 0f;
+
+	private bool foundClearArea = false;
+
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastTrigger += Time.deltaTime;
+
+		if (timeSinceLastTrigger > 1f && Time.realtimeSinceStartup > 10f && !foundClearArea) {
+			SendMessageUpwards ("OnFindClearArea");
+			foundClearArea = true;
+		}
+	}
+
+	void OnTriggerStay (Collider collider) {
+		if (collider.tag != "Player") {
+			timeSinceLastTrigger = 0f;
+		}
+	}
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Transform playerSpawnPoints; // The parent of the spawn points
+	public GameObject landingAreaPrefab;
+
+	private bool reSpawn = false;
+	private Transform[] spawnPoints;
+	private bool lastRespawnToggle = false;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastRespawnToggle != reSpawn) {
+			Respawn ();
+			reSpawn = false;
+		} else {
+			lastRespawnToggle = reSpawn;
+		}
+	}
+
+	private void Respawn() {
+		int i = Random.Range (1, spawnPoints.Length);
+		transform.position = spawnPoints [i].transform.position;
+	}
+
+	void OnFindClearArea () {
+		Invoke ("DropFlare", 3f);
+	}
+
+	void DropFlare () {
+		Instantiate (landingAreaPrefab, transform.position, transform.rotation);
+	}
+
+}
